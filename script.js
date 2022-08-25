976e31d6935b425dcb20a611efb63cf19794b141
unknown <welison-coder>
2022-08-25 19:45:49 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,52 +1,93 @@
 let musicas = [
     {
-     titulo: 'musica aleatoria',
+     titulo: 'guitarra raiz',
      artista: 'joão tinto',
      src:'musicas/Look Both Ways - Nathan Moore.mp3',
      img: 'imagens/belen-garrido-n642zkjBAEY-unsplash.jpg'
+    },
+    {
+        titulo: 'piano solo',
+        artista: 'Raimundinho',
+        src:'musicas/Read All Over - Nathan Moore.mp3',
+        img: 'imagens/david-lozano-hPD0w4nkhXo-unsplash.jpg'
+   },
+   {
+    titulo: 'baixo no chão',
+    artista: 'fernadinho e banda',
+    src:'musicas/Teasing the King - Nathan Moore.mp3',
+    img: 'imagens/vale-arellano-vkMIlSf8lfg-unsplash.jpg'
 }
 ]
  
-let musica = document.querySelector('audio')
-let duracaoMusica = document.querySelector('.fim')
-let imagem = document.querySelector('img')
-let nomeMusica = document.querySelector('.descricao h2')
-let nomeArtista = document.querySelector('.descricao i')
-// eventos
-document.querySelector('.botao-play').addEventListener('click',tocarMusica)
+let musica = document.querySelector('audio');
+let indexMusica = 0;
 
-musica.addEventListener('timeupdate',atualizarBarra)
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
 
-duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration)) 
+renderizarMusica(indexMusica);
 
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
-// funções
-function tocarMusica(){
-    musica.play()
-    document.querySelector('.botao-pause').style.display = 'block'
-    document.querySelector('.botao-play').style.display = 'none'
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proximo').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
 }
 
-document.querySelector('.botao-pause').addEventListener('click',pusarMusica)
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
 
-function pusarMusica(){
-    musica.pause()
-    document.querySelector('.botao-pause').style.display = 'none'
-    document.querySelector('.botao-play').style.display = 'block'
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
 }
 
 function atualizarBarra(){
-    let barra = document.querySelector('progress')
-    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
-    let tempoDecorrido = document.querySelector('.inicio')
-    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime)) 
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
 function segundosParaMinutos(segundos){
-    let campoMinutos = Math.floor(segundos / 60)
-    let campoSegundos = segundos % 60
-    if(campoSegundos < 10){
-        campoSegundos = '0' + campoSegundos
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
     }
-    return campoMinutos+':'+campoSegundos
+
+    return campoMinutos+':'+campoSegundos;
 }
